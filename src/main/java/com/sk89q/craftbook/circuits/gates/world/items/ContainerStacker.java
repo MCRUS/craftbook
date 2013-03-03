a67a59724fb7a85ee8173cbc4487efbf49c03df5
Alex Bond <alex.bondarevskiy@gmail.com>
2013-03-03 22:09:06 +0200

    fix undefined symbol

--- src/main/java/com/sk89q/craftbook/circuits/gates/world/items/ContainerStacker.java
+++ src/main/java/com/sk89q/craftbook/circuits/gates/world/items/ContainerStacker.java
@@ -1,22 +1,18 @@
 package com.sk89q.craftbook.circuits.gates.world.items;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.sk89q.craftbook.ChangedSign;
+import com.sk89q.craftbook.bukkit.util.BukkitUtil;
+import com.sk89q.craftbook.circuits.ic.*;
+import com.sk89q.craftbook.util.ItemUtil;
+import com.sk89q.craftbook.util.SignUtil;
+import com.sk89q.util.yaml.YAMLProcessor;
 import org.bukkit.Server;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
-import com.sk89q.craftbook.ChangedSign;
-import com.sk89q.craftbook.bukkit.util.BukkitUtil;
-import com.sk89q.craftbook.circuits.ic.AbstractIC;
-import com.sk89q.craftbook.circuits.ic.AbstractICFactory;
-import com.sk89q.craftbook.circuits.ic.ChipState;
-import com.sk89q.craftbook.circuits.ic.IC;
-import com.sk89q.craftbook.circuits.ic.ICFactory;
-import com.sk89q.craftbook.util.ItemUtil;
-import com.sk89q.craftbook.util.SignUtil;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContainerStacker extends AbstractIC {
 
