efc1dfd5f370a9421a7bf3aa86bea9cdf55eb63f
Alex Bond <alex.bondarevskiy@gmail.com>
2013-03-16 02:30:26 +0200

    Merge remote-tracking branch 'sk89q/master'
    Conflicts:
    	src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
    	src/main/java/com/sk89q/craftbook/circuits/gates/world/items/ContainerStacker.java

--- src/main/java/com/sk89q/craftbook/circuits/gates/world/items/ContainerStacker.java
+++ src/main/java/com/sk89q/craftbook/circuits/gates/world/items/ContainerStacker.java
@@ -11,16 +11,8 @@ import org.bukkit.block.Block;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
-import com.sk89q.craftbook.ChangedSign;
-import com.sk89q.craftbook.bukkit.util.BukkitUtil;
-import com.sk89q.craftbook.circuits.ic.AbstractICFactory;
-import com.sk89q.craftbook.circuits.ic.AbstractSelfTriggeredIC;
-import com.sk89q.craftbook.circuits.ic.ChipState;
-import com.sk89q.craftbook.circuits.ic.IC;
-import com.sk89q.craftbook.circuits.ic.ICFactory;
-import com.sk89q.craftbook.util.ItemUtil;
-import com.sk89q.craftbook.util.SignUtil;
-import com.sk89q.util.yaml.YAMLProcessor;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContainerStacker extends AbstractSelfTriggeredIC {
 
